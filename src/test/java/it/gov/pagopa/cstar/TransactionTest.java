a2e4b90054a5cea439d4031bd8d100980caf4454
Giovanni <g.mancini.0@gmail.com>
2022-05-10 15:24:45 +0200

    [RTD-40] Add basic repo structure

---
+++ src/test/java/it/gov/pagopa/cstar/TransactionTest.java
@@ -0,0 +1,19 @@
+package it.gov.pagopa.cstar;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class TransactionTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
